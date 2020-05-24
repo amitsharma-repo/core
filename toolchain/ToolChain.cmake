77276767a5634277c5240563e5955a73c4812e49
ultimatepandit <er.absharma198@gmail.com>
2020-05-25 01:32:10 +0530

    adding bootstrap and toolchain.cmake

---
+++ toolchain/ToolChain.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_CXX_COMPILER /usr/bin/g++-7)
+set(CMAKE_C_COMPILER /usr/bin/gcc)
+#set(compiler_root /usr/bin)
+#set(glibc_root /usr/bin)
+#set(butils_root /usr/bin)
+#set(CMAKE_C_COMPILER ${compiler_root}/gcc)
+#set(CMAKE_CXX_COMPILER ${compiler_root}/g++-7)
+#set(CMAKE_EXE_LINKER_FLAGS_INIT "-WL, -rpath, ${glibc_root}/lib:${compiler_root}/lib64:/usr/lib64")
+
+#set(BOOST_INCLUDEDIR )
+#set(BOOST_LIBRARYDIR )
